beb08ffa87863c66d06aa5ad1de9202be22a71ac
Chris-labtech <78648563+Chris-labtech@users.noreply.github.com>
2025-04-07 23:17:00 +0200

    Side Panel Transition
    Added Transition feature to side panel.
    Bug on the Menu Panel

--- Offline App - Asset Management/homePage.Designer.cs
+++ Offline App - Asset Management/homePage.Designer.cs
@@ -29,174 +29,182 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.panelTop = new System.Windows.Forms.Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(homePage));
+            this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtSidePanelExpansion = new System.Windows.Forms.PictureBox();
-            this.btnLogout = new System.Windows.Forms.Button();
-            this.btnDashboard = new System.Windows.Forms.Button();
-            this.btnSettings = new System.Windows.Forms.Button();
-            this.panelMenuExpand = new System.Windows.Forms.Panel();
-            this.btnCurrency = new System.Windows.Forms.Button();
+            this.txtDropDown = new System.Windows.Forms.PictureBox();
+            this.sidePanel = new System.Windows.Forms.Panel();
+            this.menuPanel = new System.Windows.Forms.Panel();
             this.btnMenu = new System.Windows.Forms.Button();
             this.btnCrypto = new System.Windows.Forms.Button();
-            this.timerLeftPanelTransition = new System.Windows.Forms.Timer(this.components);
-            this.panelTop.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.txtSidePanelExpansion)).BeginInit();
-            this.txtSidePanelExpansion.SuspendLayout();
-            this.panelMenuExpand.SuspendLayout();
+            this.btnCurrency = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.sidePanelTransition = new System.Windows.Forms.Timer(this.components);
+            this.menuPanelTransition = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDropDown)).BeginInit();
+            this.sidePanel.SuspendLayout();
+            this.menuPanel.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panelTop
+            // panel1
             // 
-            this.panelTop.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.panelTop.Controls.Add(this.label1);
-            this.panelTop.Controls.Add(this.txtSidePanelExpansion);
-            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTop.Location = new System.Drawing.Point(0, 0);
-            this.panelTop.Name = "panelTop";
-            this.panelTop.Size = new System.Drawing.Size(852, 48);
-            this.panelTop.TabIndex = 0;
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtDropDown);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1158, 56);
+            this.panel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.White;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(42, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 22);
-            this.label1.TabIndex = 2;
+            this.label1.Size = new System.Drawing.Size(131, 25);
+            this.label1.TabIndex = 1;
             this.label1.Text = "Data Wealth";
             // 
-            // txtSidePanelExpansion
+            // txtDropDown
             // 
-            this.txtSidePanelExpansion.BackColor = System.Drawing.SystemColors.Window;
-            this.txtSidePanelExpansion.Location = new System.Drawing.Point(12, 12);
-            this.txtSidePanelExpansion.Name = "txtSidePanelExpansion";
-            this.txtSidePanelExpansion.Size = new System.Drawing.Size(24, 18);
-            this.txtSidePanelExpansion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.txtSidePanelExpansion.TabIndex = 2;
-            this.txtSidePanelExpansion.TabStop = false;
+            this.txtDropDown.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtDropDown.Image = ((System.Drawing.Image)(resources.GetObject("txtDropDown.Image")));
+            this.txtDropDown.Location = new System.Drawing.Point(12, 12);
+            this.txtDropDown.Name = "txtDropDown";
+            this.txtDropDown.Size = new System.Drawing.Size(34, 32);
+            this.txtDropDown.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.txtDropDown.TabIndex = 1;
+            this.txtDropDown.TabStop = false;
+            this.txtDropDown.Click += new System.EventHandler(this.txtDropDown_Click);
             // 
-            // txtSidePanelExpansion
+            // sidePanel
             // 
-            this.txtSidePanelExpansion.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.txtSidePanelExpansion.Controls.Add(this.btnLogout);
-            this.txtSidePanelExpansion.Controls.Add(this.btnDashboard);
-            this.txtSidePanelExpansion.Controls.Add(this.btnSettings);
-            this.txtSidePanelExpansion.Controls.Add(this.panelMenuExpand);
-            this.txtSidePanelExpansion.Dock = System.Windows.Forms.DockStyle.Left;
-            this.txtSidePanelExpansion.Location = new System.Drawing.Point(0, 48);
-            this.txtSidePanelExpansion.Name = "panelLeftSide";
-            this.txtSidePanelExpansion.Size = new System.Drawing.Size(230, 430);
-            this.txtSidePanelExpansion.TabIndex = 1;
+            this.sidePanel.BackColor = System.Drawing.Color.LightGray;
+            this.sidePanel.Controls.Add(this.menuPanel);
+            this.sidePanel.Controls.Add(this.btnAbout);
+            this.sidePanel.Controls.Add(this.btnLogout);
+            this.sidePanel.Controls.Add(this.btnDashboard);
+            this.sidePanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidePanel.Location = new System.Drawing.Point(0, 56);
+            this.sidePanel.Name = "sidePanel";
+            this.sidePanel.Size = new System.Drawing.Size(210, 539);
+            this.sidePanel.TabIndex = 1;
+            // 
+            // menuPanel
+            // 
+            this.menuPanel.BackColor = System.Drawing.SystemColors.ControlText;
+            this.menuPanel.Controls.Add(this.btnMenu);
+            this.menuPanel.Controls.Add(this.btnCrypto);
+            this.menuPanel.Controls.Add(this.btnCurrency);
+            this.menuPanel.Location = new System.Drawing.Point(12, 52);
+            this.menuPanel.Name = "menuPanel";
+            this.menuPanel.Size = new System.Drawing.Size(180, 137);
+            this.menuPanel.TabIndex = 3;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMenu.Location = new System.Drawing.Point(3, 3);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(171, 40);
+            this.btnMenu.TabIndex = 2;
+            this.btnMenu.Text = "MENU";
+            this.btnMenu.UseVisualStyleBackColor = false;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
+            // 
+            // btnCrypto
+            // 
+            this.btnCrypto.Location = new System.Drawing.Point(3, 49);
+            this.btnCrypto.Name = "btnCrypto";
+            this.btnCrypto.Size = new System.Drawing.Size(171, 40);
+            this.btnCrypto.TabIndex = 2;
+            this.btnCrypto.Text = "Crypto";
+            this.btnCrypto.UseVisualStyleBackColor = true;
+            // 
+            // btnCurrency
+            // 
+            this.btnCurrency.Location = new System.Drawing.Point(3, 95);
+            this.btnCurrency.Name = "btnCurrency";
+            this.btnCurrency.Size = new System.Drawing.Size(171, 40);
+            this.btnCurrency.TabIndex = 2;
+            this.btnCurrency.Text = "Currency";
+            this.btnCurrency.UseVisualStyleBackColor = true;
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(12, 441);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(171, 40);
+            this.btnAbout.TabIndex = 2;
+            this.btnAbout.Text = "About";
+            this.btnAbout.UseVisualStyleBackColor = true;
             // 
             // btnLogout
             // 
-            this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogout.Location = new System.Drawing.Point(12, 122);
+            this.btnLogout.Location = new System.Drawing.Point(12, 487);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(150, 30);
-            this.btnLogout.TabIndex = 3;
+            this.btnLogout.Size = new System.Drawing.Size(171, 40);
+            this.btnLogout.TabIndex = 2;
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = true;
             // 
             // btnDashboard
             // 
-            this.btnDashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDashboard.Location = new System.Drawing.Point(12, 6);
             this.btnDashboard.Name = "btnDashboard";
-            this.btnDashboard.Size = new System.Drawing.Size(150, 30);
-            this.btnDashboard.TabIndex = 3;
+            this.btnDashboard.Size = new System.Drawing.Size(171, 40);
+            this.btnDashboard.TabIndex = 2;
             this.btnDashboard.Text = "Dashboard";
             this.btnDashboard.UseVisualStyleBackColor = true;
             // 
-            // btnSettings
+            // sidePanelTransition
             // 
-            this.btnSettings.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSettings.Location = new System.Drawing.Point(12, 86);
-            this.btnSettings.Name = "btnSettings";
-            this.btnSettings.Size = new System.Drawing.Size(150, 30);
-            this.btnSettings.TabIndex = 3;
-            this.btnSettings.Text = "Settings";
-            this.btnSettings.UseVisualStyleBackColor = true;
+            this.sidePanelTransition.Interval = 10;
+            this.sidePanelTransition.Tick += new System.EventHandler(this.sidePanelTransition_Tick);
             // 
-            // panelMenuExpand
+            // menuPanelTransition
             // 
-            this.panelMenuExpand.BackColor = System.Drawing.Color.Gainsboro;
-            this.panelMenuExpand.Controls.Add(this.btnCurrency);
-            this.panelMenuExpand.Controls.Add(this.btnMenu);
-            this.panelMenuExpand.Controls.Add(this.btnCrypto);
-            this.panelMenuExpand.Location = new System.Drawing.Point(5, 42);
-            this.panelMenuExpand.Name = "panelMenuExpand";
-            this.panelMenuExpand.Size = new System.Drawing.Size(157, 38);
-            this.panelMenuExpand.TabIndex = 2;
-            // 
-            // btnCurrency
-            // 
-            this.btnCurrency.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCurrency.Location = new System.Drawing.Point(3, 75);
-            this.btnCurrency.Name = "btnCurrency";
-            this.btnCurrency.Size = new System.Drawing.Size(150, 30);
-            this.btnCurrency.TabIndex = 3;
-            this.btnCurrency.Text = "Currency";
-            this.btnCurrency.UseVisualStyleBackColor = true;
-            // 
-            // btnMenu
-            // 
-            this.btnMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMenu.Location = new System.Drawing.Point(3, 4);
-            this.btnMenu.Name = "btnMenu";
-            this.btnMenu.Size = new System.Drawing.Size(150, 30);
-            this.btnMenu.TabIndex = 3;
-            this.btnMenu.Text = "Menu";
-            this.btnMenu.UseVisualStyleBackColor = true;
-            // 
-            // btnCrypto
-            // 
-            this.btnCrypto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCrypto.Location = new System.Drawing.Point(3, 40);
-            this.btnCrypto.Name = "btnCrypto";
-            this.btnCrypto.Size = new System.Drawing.Size(150, 30);
-            this.btnCrypto.TabIndex = 3;
-            this.btnCrypto.Text = "Crypto";
-            this.btnCrypto.UseVisualStyleBackColor = true;
-            // 
-            // timerLeftPanelTransition
-            // 
-            this.timerLeftPanelTransition.Interval = 10;
+            this.menuPanelTransition.Interval = 20;
+            this.menuPanelTransition.Tick += new System.EventHandler(this.menuPanelTransition_Tick);
             // 
             // homePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(852, 478);
-            this.Controls.Add(this.panelLeftSide);
-            this.Controls.Add(this.panelTop);
+            this.ClientSize = new System.Drawing.Size(1158, 595);
+            this.Controls.Add(this.sidePanel);
+            this.Controls.Add(this.panel1);
             this.Name = "homePage";
             this.Text = "homePage";
-            this.panelTop.ResumeLayout(false);
-            this.panelTop.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.txtSidePanelExpansion)).EndInit();
-            this.panelLeftSide.ResumeLayout(false);
-            this.panelMenuExpand.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDropDown)).EndInit();
+            this.sidePanel.ResumeLayout(false);
+            this.menuPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panelTop;
-        private System.Windows.Forms.Panel panelLeftSide;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox txtDropDown;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox txtSidePanelExpansion;
-        private System.Windows.Forms.Panel panelMenuExpand;
+        private System.Windows.Forms.Panel sidePanel;
         private System.Windows.Forms.Button btnDashboard;
-        private System.Windows.Forms.Button btnCurrency;
-        private System.Windows.Forms.Button btnMenu;
-        private System.Windows.Forms.Button btnCrypto;
-        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Timer sidePanelTransition;
+        private System.Windows.Forms.Button btnAbout;
         private System.Windows.Forms.Button btnLogout;
-        private System.Windows.Forms.Timer timerLeftPanelTransition;
+        private System.Windows.Forms.Button btnCurrency;
+        private System.Windows.Forms.Button btnCrypto;
+        private System.Windows.Forms.Panel menuPanel;
+        private System.Windows.Forms.Button btnMenu;
+        private System.Windows.Forms.Timer menuPanelTransition;
     }
 }
--- Offline App - Asset Management/homePage.cs
+++ Offline App - Asset Management/homePage.cs
@@ -17,5 +17,73 @@ namespace Offline_App___Asset_Management
             InitializeComponent();
         }
 
+        // If this image is clicked, the side panel will either expand or close
+        private void txtDropDown_Click(object sender, EventArgs e)
+        {
+            sidePanelTransition.Start();
+        }
+
+        //  Variables for Side Panel Expansion
+        bool sidePanelExpandState = true;
+        private void sidePanelTransition_Tick(object sender, EventArgs e)
+        {
+            if (sidePanelExpandState)
+            {
+                sidePanel.Width -= 10;
+                if(sidePanel.Width <= 50)
+                {
+                    sidePanelTransition.Stop();
+                    sidePanelExpandState = false;
+                }
+            }
+            else
+            {
+                sidePanel.Width += 10;
+                if(sidePanel.Width >= 210)
+                {
+                    sidePanelTransition.Stop();
+                    sidePanelExpandState = true;
+                }
+            }
+        }
+
+        //  Variables for Side Panel Expansion
+        bool menuPanelExpandState = false;
+
+        // If menu button is pressed, the menu will either expand or close
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            //menuPanelExpandState = true;
+            //MessageBox.Show("Menu button was clicked");
+            menuPanelTransition.Start();
+        }
+
+
+        private void menuPanelTransition_Tick(object sender, EventArgs e)
+        {
+            
+
+            if (menuPanelExpandState)
+            {
+                // Collapse
+                menuPanel.Height -= 10;
+                if (menuPanel.Height <= 50)
+                {
+                    menuPanelTransition.Stop();
+                    menuPanelExpandState = false;
+                }
+            }
+            else
+            {
+                // Expand
+                menuPanel.Height += 10;
+                if (menuPanel.Height >= 137)
+                {
+                    menuPanelTransition.Stop();
+                    menuPanelExpandState = true;
+                }
+            }
+        }
+
     }
 }
