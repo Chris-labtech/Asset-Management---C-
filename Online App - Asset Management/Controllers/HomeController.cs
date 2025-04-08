b02ecb9fb4c3816c5364680aa4d71ca7fafdd930
Chris-labtech <78648563+Chris-labtech@users.noreply.github.com>
2025-04-08 08:06:30 +0200

    Online Application created
    Files created for Online Application - MVC Pattern Used

---
+++ Online App - Asset Management/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Online_App___Asset_Management.Models;
+
+namespace Online_App___Asset_Management.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Online App - Asset Management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Online_App___Asset_Management.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
