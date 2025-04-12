22e64d96c64538d6c41f150e8c651eec6ddf8b7a
Chris-labtech <78648563+Chris-labtech@users.noreply.github.com>
2025-04-12 17:31:15 +0200

    Created Database - EF Core Framework
    Created Database Related functions. Installed pgAdmin. Using Enitity Framework as a Process for setting up the DB. Tested using migration.

---
+++ AssetManagement_DataBase/AssetDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+
+namespace AssetManagement_DataBase
+{
+    public class AssetDbContextFactory : IDesignTimeDbContextFactory<AssetDbContext>
+    {
+        public AssetDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AssetDbContext>();
+            optionsBuilder.UseNpgsql("Host=localhost;Database=AssetDb;Username=postgres;Password=1234");
+
+            return new AssetDbContext(optionsBuilder.Options);
+        }
+    }
+}
