026c96bbefb1d2f16ea51acd296b94ab2b0c6965
Chris-labtech <78648563+Chris-labtech@users.noreply.github.com>
2025-04-13 12:53:44 +0200

    Back End - API Commands - Swagger
    Created the API commands (v1) - GET/POST/GET/PUT/DELETE.
    There was an issue added a PATCH command.

---
+++ AssetManagement_CommonLibrary/Models/Asset.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement_CommonLibrary.Models
+{
+    public class Asset
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public required string Category { get; set; }
+        public float Value { get; set; }
+        public required string Currency { get; set; }
+        public DateTime DateCreatedInDb { get; set; }
+        public required string Location { get; set; }
+    }
+}
---
+++ AssetManagement_DataBase/AssetDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssetManagement_CommonLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AssetManagement_DataBase
+{
+    public class AssetDbContext : DbContext
+    {
+        public DbSet<Asset> Assets { get; set; }
+
+        public AssetDbContext(DbContextOptions<AssetDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Password as per pgAdmin is '1234'
+                optionsBuilder.UseNpgsql("Host=localhost;Database=AssetDb;Username=postgres;Password=1234");
+            }
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Asset>().HasData(
+                new Asset
+                {
+                    Id = "1", 
+                    Name = "Office Chair",
+                    Category = "Furniture",
+                    Value = 1000,
+                    Currency = "ZAR",
+                    DateCreatedInDb = DateTime.SpecifyKind(new DateTime(2025, 1, 1), DateTimeKind.Utc),
+                    Location = "Main Office"
+                }
+            );
+        }
+    }
+}
