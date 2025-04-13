c6dfbb5b2a767714e5a132aa95e2056a6ebd6ac8
Chris-labtech <78648563+Chris-labtech@users.noreply.github.com>
2025-04-13 23:05:40 +0200

    Delete Online App - Asset Management directory
    New Directory was created - 'AssetManagement_WebApplication'.

--- Online App - Asset Management/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Online_App___Asset_Management.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
