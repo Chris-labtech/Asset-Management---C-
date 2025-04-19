0c97fa11716436b551baf73ffa5c687aaa85366e
Chris-labtech <78648563+Chris-labtech@users.noreply.github.com>
2025-04-19 20:37:11 +0200

    New API folder and Files

---
+++ AssetManagement_APIs/API.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssetManagement_DataBase;
+using AssetManagement_CommonLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+
